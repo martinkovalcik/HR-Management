2dc540a7915151f5a8edf7d93b3a46855d8a24a5
martinkovalcik <koviyolo@gmailcom>
2021-03-02 09:54:27 +0100

    first story

---
+++ src/main/java/sk/kosickaakademia/kovalcik/company/App.java
@@ -0,0 +1,13 @@
+package sk.kosickaakademia.kovalcik.company;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
